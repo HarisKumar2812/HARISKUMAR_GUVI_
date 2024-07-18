7c2a52f97c5d97dce85c19c53ca4a880da232868
HarisKumar2812 <161690098+HarisKumar2812@users.noreply.github.com>
2024-07-18 13:33:32 +0530

    login.php

--- php/login.php
+++ php/login.php
@@ -1,18 +1,27 @@
 <?php
-require 'db.php';
-require 'session.php';
+require_once "../_config/db.php";
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$email = $_POST['gmail'];
+$password = $_POST['gpassword'];
+$sql = "SELECT * FROM user_details WHERE email=? and password = ?"; // SQL with parameters
+$stmt = $con->prepare($sql); 
+$stmt->bind_param("ss", $email, $password);
+$stmt->execute();
+$result = $stmt->get_result(); 
+$user = $result->fetch_assoc(); 
 
-$sql = 'SELECT id, password FROM users WHERE username = ?';
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$username]);
-
-$user = $stmt->fetch();
-
-if ($user && password_verify($password, $user['password'])) {
-    $sessionId = createSession($user['id']);
-    echo json_encode(['success' => true, 'sessionId' => $sessionId]);
+if ($user) {
+    exit(json_encode([
+        'status' => 'success',
+        'status_code' => 200,
+        'message' => 'logged in successfully.',
+        'user' => $user
+    ]));
 } else {
-    echo json_encode(['success' => false,
+    exit(json_encode([
+        'status' => 'error',
+        'status_code' => 400,
+        'message' => 'Invalid email or password.',
+    ])); 
+}
+?>
